54b8e063b354327325c34857f247971211f81d53
Roman Navarro <ronnoverro@gmail.com>
2024-06-24 17:33:16 -0700

    more gameOver scenes

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -13,13 +13,25 @@ public class BirdScript : MonoBehaviour
 
     public bool isBird = false;
 
+    private Camera mainCamera;
+    private float cameraHeight;
+
     // Start is called before the first frame update
     void Start()
     {
+        mainCamera = Camera.main;
+
         // SHIT IN HERE RUNS AS SOON AS THE GAME STARTS. Runs only once. gameObject is basically "this":
         gameObject.name = "Pablo";
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<logicScript2>();
         // what's the point of having this when logic is already defined above?
+
+        cameraHeight = 2f * mainCamera.orthographicSize;
+        float width = cameraHeight * mainCamera.aspect;
+
+        // Print the camera's height and width
+        Debug.Log("Orthographic Camera Height: " + cameraHeight);
+        Debug.Log("Orthographic Camera Width: " + width);
     }
 
     // Update is called ONCE PER FRAME.
@@ -30,9 +42,17 @@ public class BirdScript : MonoBehaviour
             myRigidBody.velocity = Vector2.up * flapStrength;  // shorthand for Vector(0, 1)
             // how's this work? shouldn't bird keep going up as spacebar is held down?
         }
+
+        float gameObjectY = transform.position.y;
+
+        if (gameObjectY < cameraHeight)
+        {
+            logic.gameOver();
+        }
     }
 
     // the BIRD will be the trigger. This occurs when it collides with ANYTHING:
+    // This method is automatically called by Unity when the GameObject with this script attached collides with another GameObject
     private void OnCollisionEnter2D(Collision2D collision)
     {
         logic.gameOver();
